ac80920c2a41b12c3aebcf27945004b2d61b19c1
ajnr2456 <arjunnagar486@gmail.com>
2026-07-12 07:30:29 +0530

    define ride requested, matched, and failed event payloads

---
+++ ride-service/src/main/java/com/uber/rideservice/event/RideRequestedEvent.java
@@ -0,0 +1,20 @@
+package com.uber.rideservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RideRequestedEvent {
+    private String rideId;
+    private String passengerId;
+    private double pickupLatitude;
+    private double pickupLongitude;
+    private double dropLatitude;
+    private double dropLongitude;
+    private double fare;
+}
